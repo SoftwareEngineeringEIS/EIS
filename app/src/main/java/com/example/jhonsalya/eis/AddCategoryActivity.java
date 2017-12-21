3a7f3bff265d88a6b77d60670fda8c19284c3a4d
githadiari <githadiari@gmail.com>
2017-12-20 19:24:41 -0700

    Add Category

--- app/src/main/java/com/example/jhonsalya/eis/AddCategoryActivity.java
+++ app/src/main/java/com/example/jhonsalya/eis/AddCategoryActivity.java
@@ -1,13 +1,81 @@
 package com.example.jhonsalya.eis;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.jhonsalya.eis.Model.Category;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class AddCategoryActivity extends AppCompatActivity {
+
+    private EditText editName;
+
+    private StorageReference storageReference;
+    private FirebaseDatabase database;
+    private DatabaseReference databaseReference;
+
+    private FirebaseAuth mAuth;
+    private DatabaseReference mDatabaseUsers;
+    private FirebaseUser mCurrentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_category);
+
+        editName = (EditText) findViewById(R.id.editCategoryName);
+
+        storageReference = FirebaseStorage.getInstance().getReference();
+        databaseReference = database.getInstance().getReference().child("EventApp");
+
+        mAuth = FirebaseAuth.getInstance();
+        mCurrentUser = mAuth.getCurrentUser();
+        mDatabaseUsers = FirebaseDatabase.getInstance().getReference().child("Users").child(mCurrentUser.getUid());
+
+    }
+
+    public void addCategoryButtonClicked(View view){
+        final String titleValue = editName.getText().toString().trim();
+
+        if(!TextUtils.isEmpty(titleValue)){
+            final DatabaseReference newPost = databaseReference.push();
+            mDatabaseUsers.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    newPost.child("Name").setValue(titleValue).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(AddCategoryActivity.this, "Upload Complete", Toast.LENGTH_LONG).show();
+                                Intent mainActivityIntent = new Intent(AddCategoryActivity.this, Category.class);
+                                startActivity(mainActivityIntent);
+                            }
+                        }
+                    });
+
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+        }
     }
 }
